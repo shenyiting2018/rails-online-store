9812e6c039d9934127eda6c61f548ece56882173
Yiting <shenyiting2018@gmail.com>
2019-12-03 23:40:13 -0800

    restyling the products page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+  validates :name, :description, :image, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :name, uniqueness: true
+  validates :image, 
+      allow_blank: true, 
+      format: {
+          with: %r{\.(gif|jpg|png)\Z}i, 
+          message: 'must be GIF, JPG, PNG images'
+      }
 end
