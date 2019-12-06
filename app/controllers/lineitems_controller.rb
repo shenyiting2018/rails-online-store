7ce449accc7457b7fd7d391e4a9292f96a9d13d3
Yiting <shenyiting2018@gmail.com>
2019-12-05 19:58:57 -0800

    ajax

--- app/controllers/lineitems_controller.rb
+++ app/controllers/lineitems_controller.rb
@@ -32,11 +32,12 @@ class LineitemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to shopper_url }
+        format.js
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
-        format.json { render json: @lineitem.errors, status: :unprocessable_entity }    
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }    
       end
     end
   end
