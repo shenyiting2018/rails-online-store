270d5f2d1c0fbbdb8e9d8e9c230b1f9fb753aed6
Yiting <shenyiting2018@gmail.com>
2019-12-12 00:36:59 -0800

    add searching feature

--- app/controllers/shopper_controller.rb
+++ app/controllers/shopper_controller.rb
@@ -3,4 +3,13 @@ class ShopperController < ApplicationController
   def index
   	@products = Product.all
   end
+
+  def search
+  	if params.has_key?(:name) && params[:name]
+  		name = params[:name]
+  		@products = Product.where('name LIKE ?', "%#{name}%")
+  	else
+  		@products = Product.all?
+  	end
+  end
 end
